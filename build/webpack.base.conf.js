cefc15637e7c8f7ae14e6b9bad1f8a6156867f5a
afeiship <1290657123@qq.com>
2018-06-04 11:04:10 +0800

    issue: bugfix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -16,6 +16,7 @@ var externals = process.env.NODE_ENV === 'production' ? {
   'classnames': 'classnames',
   'react-dom': 'react-dom',
   'noop':'noop',
+  'next-return-event':'next-return-event',
   'object-assign':'object-assign',
   'prop-types':'prop-types',
 } : {};
